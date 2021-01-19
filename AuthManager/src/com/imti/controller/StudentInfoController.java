843ca3689cafc085bfdad585f279a1114d434be9
duyonggang <2509634442@qq.com>
2021-01-19 22:54:47 +0800

    添加学生controller层

---
+++ AuthManager/src/com/imti/controller/StudentInfoController.java
@@ -0,0 +1,16 @@
+package com.imti.controller;
+/**@文件名: StudentInfoController.java
+ * @类功能说明: 
+ * @作者: DuYongGang
+ * @Email: 2509634442@qq.com
+ * @日期: 2021年1月19日下午10:53:37
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: DuYongGang</li> 
+ * 	 <li>日期: 2021年1月19日下午10:53:37</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class StudentInfoController {
+
+}
